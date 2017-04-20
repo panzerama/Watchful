ee941e02d7ca701e7b666c1ef292cf79f25cf3f0
JD Panzer <jason.drew.panzer@gmail.com>
2017-04-20 08:35:59 -0700

    updated first activity to take text input, button placeholder

--- app/src/main/java/com/indexyear/jd/watchful/WelcomeActivity.java
+++ app/src/main/java/com/indexyear/jd/watchful/WelcomeActivity.java
@@ -2,24 +2,28 @@ package com.indexyear.jd.watchful;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.widget.Button;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
 
-public class InfoActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "WelcomeActivity: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        setContentView(R.layout.activity_welcome);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button next_button = (Button) findViewById(R.id.next_button);
+        next_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
