d610fd422080e22d284cdbee66bb86b68cee9589
JD Panzer <jason.drew.panzer@gmail.com>
2017-05-04 16:39:55 -0700

    next up, creating

--- app/src/main/java/com/indexyear/jd/watchful/RecyclerActivity.java
+++ app/src/main/java/com/indexyear/jd/watchful/RecyclerActivity.java
@@ -1,29 +1,45 @@
 package com.indexyear.jd.watchful;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.widget.RelativeLayout;
 
 public class RecyclerActivity extends AppCompatActivity {
+
+    Context context;
+    RecyclerView recyclerView;
+    RelativeLayout relativeLayout;
+    RecyclerView.Adapter recyclerViewAdapter;
+    RecyclerView.LayoutManager recylerViewLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_recycler);
+
+        context = getApplicationContext();
+
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativelayout1);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview1);
+
+        recylerViewLayoutManager = new LinearLayoutManager(context);
+
+        recyclerView.setLayoutManager(recylerViewLayoutManager);
+
+        recyclerViewAdapter = new RecyclerViewAdapter(context, leftStrings);
+
+        recyclerView.setAdapter(recyclerViewAdapter);
     }
 
     String[] leftStrings = {"j6BSQMKwsM", "yR632nnnve", "zx99PkfjOg", "cfR2AurdYl", "W3W9T7G8xX",
